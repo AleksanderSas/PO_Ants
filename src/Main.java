63cce74d494e5c98b0fc147717d49b10ff9f0bbf
Alek <aleksander.sas93@gmail.com>
2016-01-03 23:01:24 +0100

    dodanie testu

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello ants!");
 		try {
-			Graph graph = new Graph("tests\\test4.txt");
+			Graph graph = new Graph("tests\\test5.txt");
 			//System.out.println(graph.printDistnce());
 			System.out.println("");
 			
-			Anthill anthill = new Anthill(20, graph);
-			anthill.findPath(50);
+			Anthill anthill = new Anthill(80, graph);
+			anthill.findPath(1000);
 			System.out.println("");
 			System.out.println(anthill.getBestPath());
 			System.out.println(graph.getStartNode().toString());
