c24c31c594658a5c163d60ba21448f3eedab63a2
Alek <aleksander.sas93@gmail.com>
2016-01-10 16:47:14 +0100

    dodanie wielowatkowosci

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,18 @@ public class Main {
 		System.out.println("Hello ants!");
 		//Graph graph = new Graph("tests\\test1.txt");
 		Graph graph = new Graph(FactorCentre.graphSize);
-		/*graph.saveGraph2XML("ttt.xml");
+		graph.saveGraph2XML("ttt.xml");
 		//System.out.println(graph.printDistnce());
 		System.out.println("");
 		
+		/*long millis = System.currentTimeMillis();
 		Anthill anthill = new Anthill(FactorCentre.antNumber, graph);
 		anthill.findPath(FactorCentre.epocheNumber, FactorCentre.maxBestRepets, 
 				(r, i) -> System.out.println(String.format("%d iter. : best distance: %d",i, r)));
 		System.out.println("");
 		System.out.println(graph.getStartNode().toString());
 		System.out.println(anthill.getBestPath());
+		System.out.println("TIME: " + (System.currentTimeMillis() - millis));
 		//System.out.println(anthill.getAllPaths());*/
 		
 		AnthillTester anthillTester = new AnthillTester("D:\\studia\\antTest2.txt", graph);
