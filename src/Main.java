ea2cc49af3eb8fed71f7ce1ee0b2e42914c1e231
Cyryl Karpiński <cyryl28@gmail.com>
2016-01-16 12:08:53 +0100

    Jeszcze raz parser
    jw.

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,9 @@ import org.dom4j.DocumentException;
 public class Main {
 
 	public static void main(String[] args) throws IOException, InternalException, DocumentException, GraphBuildException {
-		//System.out.println("Hello ants!");
+		//Wybierz plik do testowania
 		Graph graph = new Graph("tests\\test6.xml", 2);
-		//Graph graph = new Graph(FactorCentre.graphSize);
-		/*graph.saveGraph2XML("ttt.xml");*/
-		//System.out.println(graph.printDistnce());
-                /*Float f = new Float("2.000000000000000e+00");
-                float f1 = (float) f;
-                int f2 = (int) f1;
-                System.out.println(f2);*/
-		//System.out.println("");
+
 		
 		Anthill anthill = new Anthill(FactorCentre.antNumber, graph);
 		anthill.findPath(FactorCentre.epocheNumber, FactorCentre.maxBestRepets, 
@@ -22,11 +15,7 @@ public class Main {
 		System.out.println("");
 		System.out.println(graph.getStartNode().toString());
 		System.out.println(anthill.getBestPath());
-		//System.out.println(anthill.getAllPaths());*/
-		
-		//AnthillTester anthillTester = new AnthillTester("D:\\studia\\antTest2.txt", graph);
-		//anthillTester.test();
-		//anthillTester.close();
+
 
 	}
 
